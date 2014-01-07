2229d0b06c796a076061d2bac545fb9611fe69d5
Wladimir Ohrelianov II <woii@SCstage.sebkhachott.net>
2014-01-08 00:14:40 +0100

    Beginning of sequences writing

--- Mididings/pedalBoards.py
+++ Mididings/pedalBoards.py
@@ -10,7 +10,7 @@ import liblo
 config(
 	backend='jack',
 	client_name='PedalBoardsRoutes',
-	out_ports=['PBseq24', 'PBAMSBassSynth', 'PBAMSChordsSynth', 'PBAMSLeadSynth', 'PBAMSCtLeadSynth', 'PBAMSClassicalSynth', 'PBTapeutape', 'PBCtrlOut'],
+	out_ports=['PBseq24', 'PBAMSBassSynth', 'PBAMSChordsSynth', 'PBAMSLeadSynth', 'PBAMSCtLeadSynth', 'PBAMSClassicalSynth', 'PBTapeutape', 'PBGuitarDag', 'PBGuitarORL', 'PBCtrlOut'],
 	in_ports=['PBCtrlIn']
 )
 
@@ -20,9 +20,11 @@ hook(
     AutoRestart()
 )
 
+
+#### Ports OSC ################################################
+
 klickport = 1234
 slport = 9951
-ardourport = 3819
 testport = 1111
 
 
@@ -37,12 +39,14 @@ alead=Output('PBAMSLeadSynth',5)
 actlead=Output('PBAMSCtLeadSynth',6)
 aclass=Output('PBAMSClassicalSynth',7)
 
-
 tapeutape=Output('PBTapeutape',10)
 
-
+guitardag=Output('PBGuitarDag', 1)
+guitarorl=Output('PBGuitarORL', 1)
 
 #### Functions #############################################
+
+
 
 #### Trigger seq24 ####
 p_firstpart=[range(1,65)]
@@ -68,6 +72,32 @@ seqtrigger = Filter(PROGRAM) >> [
 cseqtrigger = Channel(1) >> seqtrigger
 
 
+#### Stop ####
+
+stop = [
+        Program(2) >> cseqtrigger,
+#        vxfx_ohreland,
+        SendOSC(slport, '/sl/-1/hit', 'pause_on') >> Discard(),
+        SendOSC(klickport, '/klick/metro/stop') >> Discard(),
+
+	Program(2) >> abass,
+	Program(6) >> actlead
+]
+
+
+#### Guitars ####
+
+# Dag
+gtrdagclean = Program(3) >> guitardag
+gtrdagdisto = Program(2) >> guitardag
+# gtrdagmute = SendOSC()
+
+# ORL
+gtrorlclean = Program(3) >> guitarorl
+gtrorldisto = Program(2) >> guitarorl
+# gtrdagmute = SendOSC()
+
+
 
 
 #### Scenes ################################################
@@ -76,14 +106,89 @@ run(
     scenes = {
         1: SceneGroup("Dummy", [
   		Scene("Bass ORL",
-		    ProgramFilter(2) >> [ # Intro
-			Program(65) >> cseqtrigger,
-			Program(10) >> achords,
+		    PortFilter('PBCtrlIn') >> [ 
+			ProgramFilter(1) >> stop, # !!!STOP!!! #
+			ProgramFilter(2) >> [ # Intro - Bouton 2
+			    Program(65) >> cseqtrigger,
+			    Program(10) >> achords,
+			    Program(2) >> abass,
+			    Program(6) >> actlead,
 
-			SendOSC(slport, '/set', 'eight_per_cycle', 24),
-			SendOSC(slport, '/set', 'tempo', 120),
+			    SendOSC(slport, '/set', 'eight_per_cycle', 24),
+			    SendOSC(slport, '/set', 'tempo', 120),
+
+			    SendOSC(klickport, '/klick/simple/set_tempo', 120),
+			    SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+			    SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
+			    SendOSC(klickport, '/klick/metro/start'),
+
+			    gtrdagclean
+			],
+		        ProgramFilter(3) >> [ # Intro Up - Bouton 3
+			    Program(6) >> Channel(2) >> seqtrigger,
+		        ],
+			ProgramFilter(4) >> [ # Bustas - Bouton 4
+			    Program(66) >> cseqtrigger,
+			    Program(10) >> achords,
+			    Program(1) >> abass,
+			    Program(6) >> actlead,
+
+			    SendOSC(slport, '/set', 'eight_per_cycle', 24),
+			    SendOSC(slport, '/set', 'tempo', 120),
+
+			    SendOSC(klickport, '/klick/simple/set_tempo', 120),
+			    SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+			    SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
+			    SendOSC(klickport, '/klick/metro/start'),
+
+			    gtrdagclean
+			],
+			ProgramFilter(5) >> [ # Post Bustas - Bouton 5
+			    Program(67) >> cseqtrigger,
+
+			    Program(10) >> achords,
+			    Program(1) >> abass,
+			    Program(9) >> alead,
+			    Program(6) >> actlead,
+
+			    SendOSC(slport, '/set', 'eight_per_cycle', 24),
+			    SendOSC(slport, '/set', 'tempo', 120),
+
+			    SendOSC(klickport, '/klick/simple/set_tempo', 120),
+			    SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+			    SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
+			    SendOSC(klickport, '/klick/metro/start'),
+
+			    gtrdagclean
+			],
+		        ProgramFilter(6) >> [ # Final Couplet - Bouton 6
+			    [
+				Program(19),
+				Program(23)
+			    ] >> Channel(2) >> seqtrigger,
+		        ],
+			ProgramFilter(7) >> [ # Debut Riff MathoMagma - Bouton 7
+			    stop,
+
+			    SendOSC(slport, '/set', 'eight_per_cycle', 16),
+			    SendOSC(slport, '/set', 'tempo', 120),
+
+			    SendOSC(slport, '/sl/2/hit', 'record'),
+
+			    SendOSC(klickport, '/klick/simple/set_tempo', 120),
+			    SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+			    SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
+			    SendOSC(klickport, '/klick/metro/start'),
+
+			    gtrdagclean,
+			    gtrorlclean
+
+			]
+
+
 
 		    ]
+
 		),
 		Scene("Guitar ORL",
 		    Discard()
